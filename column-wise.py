d9a9960808b2170730ccadddde01d27a3ee050ca
Rampachan <88670722+Rampachan@users.noreply.github.com>
2023-07-18 22:44:03 +0530

    column-wise.py

---
+++ column-wise.py
@@ -0,0 +1,19 @@
+prograd assignment problem
+You have given a 2D integer matrix A,return a 1D integer array containing column-wise sums of original matrix.
+
+def column_sums(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    sums = [0] * cols
+
+    for j in range(cols):
+        for i in range(rows):
+            sums[j] += matrix[i][j]
+
+    return sums
+
+
+# Example usage
+matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 2, 3, 4]]
+result = column_sums(matrix)
+print(result)
